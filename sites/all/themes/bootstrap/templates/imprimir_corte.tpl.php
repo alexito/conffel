550141038a153523d7acd953068fa5013ab19349
Alexis Saransig <asaransig@metrodigi.com>
2014-12-12 02:49:24 -0500

    print

---
+++ sites/all/themes/bootstrap/templates/imprimir_corte.tpl.php
@@ -0,0 +1,128 @@
+<div class="imprimir-corte">
+  <div class="col-sm-12 text-center">
+    CONFFEL <br> ORDEN DE TRAZO
+  </div>
+  <span class="col-sm-6">
+    <table class="table table-bordered">
+      <tr>
+        <td>TRAZADOR</td>
+        <td>FECHA DE TRAZO</td>
+      </tr>
+      <tr>
+        <td> </td>
+        <td> </td>
+      </tr>
+    </table>
+  </span>
+  <span class="col-sm-6">
+    <table class="table table-bordered">
+      <tr>
+        <td>TRAZADOR</td>
+        <td>FECHA DE TRAZO</td>
+      </tr>
+      <tr>
+        <td> </td>
+        <td> </td>
+      </tr>
+    </table>
+  </span>
+
+
+
+
+  <span class="col-sm-6">
+    <table class="table table-bordered" >
+      <tr>
+        <td>ANCHO</td>    
+        <td>LONGITUD</td>
+        <td>APROVECHAMIENTO</td>
+      </tr>
+      <tr>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+      </tr>
+    </table>
+  </span>
+  <span class="col-sm-6">
+    <table class="table table-bordered" >
+      <tr>
+        <td>ANCHO</td>    
+        <td>LONGITUD</td>
+        <td>APROVECHAMIENTO</td>
+      </tr>
+      <tr>
+        <td> </td>
+        <td> </td>
+        <td> </td>
+      </tr>
+    </table>
+  </span>
+
+
+
+  <span class="col-sm-6">
+    <table class="table table-bordered">
+      <tr>
+        <td style="text-align:center;">MODELO</td>
+        <td style="text-align:center;">TALLAS</td>
+        <td style="text-align:center;">TANTOS</td>
+        <td style="text-align:center;">TOTAL</td>
+      </tr>
+      <?php for ($i = 0; $i < 10; $i++) { ?>
+        <tr>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+        </tr>
+      <?php } ?>
+    </table>
+    <table class="table table-bordered">
+      <tr>
+          <td>OBSERVACIONES</td>
+      </tr>
+      <tr>
+        <td style="height: 30px;"></td>
+      </tr>
+    </table>
+  </span>
+  <span class="col-sm-6">
+    <table class="table table-bordered">
+      <tr>
+        <td style="text-align:center;">MODELO</td>
+        <td style="text-align:center;">TALLAS</td>
+        <td style="text-align:center;">TANTOS</td>
+        <td style="text-align:center;">TOTAL</td>
+      </tr>
+      <?php for ($i = 0; $i < 10; $i++) { ?>
+        <tr>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+        </tr>
+      <?php } ?>
+    </table>
+    <table class="table table-bordered">
+      <tr>
+          <td>OBSERVACIONES</td>
+      </tr>
+      <tr>
+        <td style="height: 30px;"></td>
+      </tr>
+    </table>
+  </span>
+</div>
+<style>
+  .table{
+    margin-bottom: 1px;
+  }
+  .table>thead>tr>th, .table>tbody>tr>th, .table>tfoot>tr>th, .table>thead>tr>td, .table>tbody>tr>td, .table>tfoot>tr>td{
+    padding: 2px !important;
+    height: 12px;
+  }
+  .imprimir-corte{
+    font-size: 9px;
+  }
+</style>
