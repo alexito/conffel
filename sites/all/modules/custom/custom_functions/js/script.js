5a3e2c95b1b3fd6827926ce76e09cd45473bf825
Alexis Saransig <al.exito_x@hotmail.com>
2015-03-16 21:54:14 -0500

    updated from ubuntu

--- sites/all/modules/custom/custom_functions/js/script.js
+++ sites/all/modules/custom/custom_functions/js/script.js
@@ -3,6 +3,14 @@
     attach: function (context, settings) {
       $(window).load(function () {
 
+        //Inicializa el Calendario
+        var d = new Date();
+        $("#calendar").calendarWidget({
+          day: d.getDate(),
+          month: d.getMonth(),
+          year: d.getFullYear()
+        });
+        
         //GUARDAR DIARIO
         $('span.guardar-diario').click(function () {
           var data = {info: []};
@@ -11,21 +19,21 @@
             var mod_id = $('tbody tr.item-diario', table).attr('data-mod-id');
 
             data.info[i] = {mod_id: mod_id, empleados: [], tarjetas: '', vxm: ''};
-            
+
             $('tbody tr.item-diario', table).each(function (ind, tr) {
               nid = $('td.views-field-nid', tr).text().trim();
               vxm = $('input#vxm_' + nid, tr).val();
               ctp = $('input#ctp_' + nid, tr).val();
               rh = $('input#rh_' + nid, tr).val();
               uds = $('input#uds_' + nid, tr).val();
-              
+
               data.info[i].tarjetas = ctp;
               data.info[i].vxm = vxm;
               data.info[i].uds = uds;
-              
+
               data.info[i].empleados[ind] = {
                 emp_id: nid,
-                rh: rh                
+                rh: rh
               };
             });
 
@@ -65,11 +73,11 @@
 
         //Elimina elementos innecesarios de la impresion
         $('.print-site_name, .print-breadcrumb, .print-hr').remove();
-        
-        if(window.location.href.indexOf('/print/') > 0){
+
+        if (window.location.href.indexOf('/print/') > 0) {
           $('.remove-on-print').remove()
         }
-        
+
 
         //Configura los imputs para permitir solo numeros
         $('input[data-custom-num="1"]').each(function (i, e) {
@@ -115,6 +123,137 @@
         });
 
       });
+
+      //--------CALENDAR WIDGET
+      function calendarWidget(el, params) {
+
+        var now = new Date();
+        var thismonth = now.getMonth();
+        var thisyear = now.getYear() + 1900;
+        var day = now.getDate();
+
+        var opts = {
+          month: thismonth,
+          year: thisyear,
+          day: day
+        };
+
+        $.extend(opts, params);
+
+        var monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+        var dayNames = ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'];
+        month = i = parseInt(opts.month);
+        year = parseInt(opts.year);
+        var m = 0;
+        var table = '';
+
+        // next month
+        if (month == 11) {
+          n_month = (year+1) + '-1';
+          var next_month = '<a href="?month=' + 1 + '&amp;year=' + (year + 1) + '" title="' + monthNames[0] + ' ' + (year + 1) + '">' + monthNames[0] + ' ' + (year + 1) + '</a>';
+        } else {
+          n_month = (year) + '-' + (month + 2);
+          var next_month = '<a href="?month=' + (month + 2) + '&amp;year=' + (year) + '" title="' + monthNames[month + 1] + ' ' + (year) + '">' + monthNames[month + 1] + ' ' + (year) + '</a>';
+        }
+
+        // previous month
+        if (month == 0) {
+          p_month = (year - 1) + '-12';
+          var prev_month = '<a href="?month=' + 12 + '&amp;year=' + (year - 1) + '" title="' + monthNames[11] + ' ' + (year - 1) + '">' + monthNames[11] + ' ' + (year - 1) + '</a>';
+        } else {
+          p_month = (year) + '-' + month;
+          var prev_month = '<a href="?month=' + (month) + '&amp;year=' + (year) + '" title="' + monthNames[month - 1] + ' ' + (year) + '">' + monthNames[month - 1] + ' ' + (year) + '</a>';
+        }
+
+        table += ('<div><span class="m-back" data-month="' + p_month + '"> < </span><span class="m-next" data-month="' + n_month + '"> > </span></div>');
+
+        table += ('<h3 id="current-month">' + monthNames[month] + ' ' + year + '</h3>');
+        // uncomment the following lines if you'd like to display calendar month based on 'month' and 'view' paramaters from the URL
+        //table += ('<div class="nav-prev">'+ prev_month +'</div>');
+        //table += ('<div class="nav-next">'+ next_month +'</div>');
+        table += ('<table class="calendar-month" ' + 'id="calendar-month' + i + ' " cellspacing="0">');
+
+        table += '<tr>';
+
+        for (d = 0; d < 7; d++) {
+          table += '<th class="weekday">' + dayNames[d] + '</th>';
+        }
+
+        table += '</tr>';
+
+        var days = getDaysInMonth(month, year);
+        var firstDayDate = new Date(year, month, 1);
+        var firstDay = firstDayDate.getDay();
+
+        var prev_days = getDaysInMonth(month, year);
+        var firstDayDate = new Date(year, month, 1);
+        var firstDay = firstDayDate.getDay();
+
+        var prev_m = month == 0 ? 11 : month - 1;
+        var prev_y = prev_m == 11 ? year - 1 : year;
+        var prev_days = getDaysInMonth(prev_m, prev_y);
+        firstDay = (firstDay == 0 && firstDayDate) ? 7 : firstDay;
+
+        var i = 0;
+        for (j = 0; j < 42; j++) {
+          today = '';
+          if ((j < firstDay)) {
+            cday=(prev_days - firstDay + j + 1)
+            table += ('<td class="other-month"><span class="day"></span></td>');
+          } else if ((j >= firstDay + getDaysInMonth(month, year))) {
+            i = i + 1;
+            cday = i;
+            table += ('<td class="other-month"><span class="day"></span></td>');
+          } else {
+            if ((j - firstDay + 1) == opts.day) {
+              today = 'today'
+            }
+            cmonth = (month+1);
+            if(cmonth < 10){
+              cmonth = '0' + cmonth;
+            }
+            
+            cday = (j - firstDay + 1);
+            if(cday < 10)
+              cday = '0' + cday;
+            table += ('<td class="current-month day' + cday + ' ' + today + '"><span class="day"><a target="_blank" href="imprimir-diario/' + year +'-' + cmonth + '-' + cday +'">' + cday + '</a></span></td>');
+          }
+          if (j % 7 == 6)
+            table += ('</tr>');
+        }
+
+        table += ('</table>');
+        table += ('<hr><span style="">Haga click en una fecha para ver los detalles e Imprimir.</span>');
+
+        el.html(table);
+        
+        var d = new Date();
+        $(".m-next, .m-back").click(function(e){
+          info = $(e.target).attr('data-month').split('-');
+          $("#calendar").html('');
+          $("#calendar").calendarWidget({
+            day: d.getDate(),
+            month: info[1] - 1,
+            year: info[0]
+          });          
+        });
+      }
+
+      function getDaysInMonth(month, year) {
+        var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        if ((month == 1) && (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))) {
+          return 29;
+        } else {
+          return daysInMonth[month];
+        }
+      }
+
+
+      // jQuery plugin initialisation
+      $.fn.calendarWidget = function (params) {
+        calendarWidget(this, params);
+        return this;
+      };
 
 
     }
---
+++ sites/all/themes/bootstrap/templates/imprimir_diario.tpl.php
@@ -0,0 +1,149 @@
+<div class="imprimir-diario">
+  <div class="col-sm-12">
+    <ul class="remove-on-print">
+      <li><h5><?php print l('Imprimir Diario', 'print/imprimir-diario/' . $output->fecha); ?></h5></li>
+      <li><h5><?php print l('Ir a la lista de Diarios', 'lista-de-diario'); ?></h5></li>
+      <li><h5><?php print l('Ir a la pagina principal', 'inicio'); ?></h5></li>
+    </ul>
+  </div>
+  <div class="col-sm-12 text-center">
+    <b>CONFFEL</b>  <br> <b>DIARIO</b>
+  </div>
+  <div class="col-sm-12">
+    <h4><b>Fecha:</b>  <?php print $output->fecha; ?></h4>
+  </div>
+
+
+  <?php
+  if(isset($output->nodes)){
+  foreach ($output->nodes as $node) {
+    $wrapper = entity_metadata_wrapper('node', $node);
+    ?>
+    <div class="col-sm-12 border-dashed">
+      <span class="col-sm-6">
+      <h5><b><u><?php print $wrapper->field_modulo->name->value(); ?></u></b></h5>
+
+      <table class="table table-bordered">
+        <tr>
+          <td style="width: 120px;">
+            <b>Cod. Tarjeta  </b>
+          </td>
+          <td class="text-center">
+            <b>SAM</b>
+          </td>
+          <td class="text-center">
+            <b>UDS</b>
+          </td>
+          <td class="text-center">
+            <b>Min Prod</b>
+          </td>
+        </tr>
+        <?php
+        $tarjetas = $wrapper->field_tarjeta->value();
+
+        foreach ($tarjetas as $tarjeta) {
+          $t_wrapper = entity_metadata_wrapper('field_collection_item', $tarjeta);
+          ?>
+
+          <tr>
+            <td>
+              <?php print $t_wrapper->field_det_tarjeta->nid->value(); ?>
+            </td>
+            <td style="text-align: right;">
+              <?php print $t_wrapper->field_det_sam->value(); ?>
+            </td>
+            <td style="text-align: right;">
+              <?php print $t_wrapper->field_det_uds->value(); ?>
+            </td>
+            <td style="text-align: right;">
+              <?php print $t_wrapper->field_det_min_prod->value(); ?>
+            </td>
+
+          </tr>
+        <?php } ?>
+        <tr>
+          <td></td>
+          <td style="text-align: right;"><b>Total: </b></td>
+          <td style="text-align: right;"><?php print $wrapper->field_uds_total->value(); ?></td>
+          <td style="text-align: right;"><?php print $wrapper->field_min_prod_total->value(); ?></td>
+        </tr>
+      </table>
+      </span>
+      <span class="col-sm-6">
+        <h5><b>Productividad: </b><?php print ($wrapper->field_variable->value() * 100) . ' %'; ?></h5>
+      <table class="table table-bordered">
+        <tr>
+          <td style="width: 80px;"><b>Cod. Emp.</b></td>
+          <td><b>Nombre</b></td>
+          <td><b>Valor minuto</b></td>
+          <td><b>Real horas</b></td>
+          <td><b>US</b></td>
+        </tr>
+
+        <?php
+        $empleados = $wrapper->field_detalle->value();
+
+        foreach ($empleados as $empleado) {
+          $e_wrapper = entity_metadata_wrapper('field_collection_item', $empleado);
+          ?>
+
+          <tr>
+            <td>
+              <?php print $e_wrapper->field_empleado->nid->value(); ?>
+            </td>
+            <td>
+              <?php
+              $emp = node_load($e_wrapper->field_empleado->nid->value());
+              print $emp->title;
+              ?>
+            </td>
+            <td style="text-align: right;">
+              <?php print $e_wrapper->field_v_min->value(); ?>
+            </td>
+            <td style="text-align: right;">
+              <?php print $e_wrapper->field_horas->value(); ?>
+            </td>
+            <td style="text-align: right;">
+              <?php print $e_wrapper->field_us_subtotal->value(); ?>
+            </td>
+          </tr>
+        <?php } ?>
+
+        <tr>
+          <td></td>
+          <td></td>
+          <td style="text-align: right;"><b>Total:</b></td>
+          <td style="text-align: right;"><?php print $wrapper->field_horas_total->value(); ?></td>
+          <td style="text-align: right;"><?php print $wrapper->field_us_total->value(); ?></td>
+        </tr>
+
+      </table>
+      </span>
+    </div>
+  <div class="clearfix"></div>
+  <?php }
+  }else{
+  ?>
+  <span class="col-sm-10">
+    <h4>No se encontraron datos en la fecha especificada.</h4>
+  </span>
+  <?php }?>
+
+</div>
+
+<style>
+  .table{
+    margin-bottom: 1px;
+  }
+  .table>thead>tr>th, .table>tbody>tr>th, .table>tfoot>tr>th, .table>thead>tr>td, .table>tbody>tr>td, .table>tfoot>tr>td{
+    padding: 2px !important;
+    height: 17px;
+  }
+  .imprimir-corte{
+    font-size: 9px;
+  }
+  .border-dashed{
+    border-top: 1px dashed #c3c3c3;
+    margin-bottom: 10px;
+  }
+</style>
