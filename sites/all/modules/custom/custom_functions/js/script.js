6ee64fd98b7c1b7ee83535e23c8e8916ccb16cd9
Alexis <alexis@mail.com>
2016-01-12 01:22:17 -0500

    calculando

--- sites/all/modules/custom/custom_functions/js/script.js
+++ sites/all/modules/custom/custom_functions/js/script.js
@@ -2,9 +2,220 @@
   Drupal.behaviors.custom_functions = {
     attach: function (context, settings) {
       $(window).load(function () {
+
+        var cat = ['crt', 'cnf', 'prp', 'trm', 'cnt', 'mpq'];
+
+        // Diario
+        $('.node-diario-form .panel-body:not(.processed)').addClass('processed').each(function(){
+          var panel = this;
+          var valor_minuto = 0;
+          var panel_parent = $(panel).parent();
+
+          var grupo = getGrupo(panel_parent);
+
+          // Remueve los elementos innecesarios
+          setInterval(function() {
+            if ($('.sticky-header, .field-multiple-drag', panel).length > 0) {
+              $('.sticky-header', panel).remove();
+              $('.field-multiple-drag', panel).remove();
+            }
+          }, 500)
+
+          // Adjunta comportamiento a los campos
+          setInterval(function() {
+
+            // ---------------- TARJETA
+            $('.field-name-field-tarjeta:not(.processed)', panel).addClass('processed').each(function() {
+              var tarjeta = this;
+              $('input.form-text', tarjeta).blur(function() {
+                var nid = $(this).val().split(':');
+                nid = nid[1].split(']');
+                nid = nid[0];
+
+                $.post( "/get-tarjeta-info/" + nid, function( data ) {
+                  console.log(data)
+                  var sam = 0;
+                  if (grupo == 'corte') {
+                    sam = parseFloat(data.info.field_sam_corte.und[0].value);
+                  }
+                  if (grupo == 'confeccion') {
+                    sam = parseFloat(data.info.field_sam_confeccion.und[0].value);
+                  }
+                  if (grupo == 'preparacion') {
+                    sam = parseFloat(data.info.field_sam_preparacion.und[0].value);
+                  }
+                  if (grupo == 'terminado') {
+                    sam = parseFloat(data.info.field_sam_terminado.und[0].value);
+                  }
+                  if (grupo == 'control-de-calidad') {
+                    sam = parseFloat(data.info.field_sam_control_de_calidad.und[0].value);
+                  }
+                  if (grupo == 'empaque') {
+                    sam = parseFloat(data.info.field_sam_empaque.und[0].value);
+                  }
+
+                  $(tarjeta).closest('td').find('.field-name-field-sam input.form-text').val(sam);
+
+                  calcularPanel(panel, valor_minuto);
+                });
+              });
+            });
+
+
+          }, 500);
+
+          // Ejecuta recalculacion despues de algun cambio en el valor minuto
+          var input_vmin = $('> .field-type-number-decimal input', panel);
+
+          setAsDecimal(input_vmin);
+
+          $(input_vmin).blur(function() {
+
+            // Valor Minuto
+            if ($(this).val() == '') {
+              valor_minuto = 0;
+              $(this).val(0);
+            }
+            else {
+              try{
+                valor_minuto = parseFloat($(this).val());
+                $(this).val(valor_minuto);
+              }catch (e){
+                valor_minuto = 0;
+                $(this).val(0);
+              }
+            }
+
+            // Recalcula el panel completo
+            calcularPanel(panel, valor_minuto);
+          });
+
+        });
+
+
+
+        function calcularModulo(modulo, panel, valor_minuto) {
+          var limite_tarjetas = $('.field-name-field-tarjetas  > div > div > table.field-multiple-table > tbody > tr', modulo).length;
+          var limite_operadores = $('.field-name-field-operadores  > div > div > table.field-multiple-table > tbody > tr', modulo).length;
+
+          var total_min_prod = 0;
+          var total_uds = 0;
+          $('.field-name-field-tarjetas  > div > div > table.field-multiple-table > tbody > tr', modulo).each(function (i, e) {
+
+            // --------------------- Calculando Tarjetas
+            var tarjeta = this;
+
+            var uds = parseInt($('.field-name-field-uds input.form-text', tarjeta).val());
+            var sam = parseFloat($('.field-name-field-sam input.form-text', tarjeta).val());
+
+            var min_prod = parseFloat(uds * sam);
+
+            total_uds += uds;
+            total_min_prod += parseFloat(min_prod);
+
+            $('.field-name-field-min-prod input.form-text', tarjeta).val(min_prod);
+
+            if ((i + 1) === limite_tarjetas) {
+              // Guarda los valores totales de la tarjeta
+              $('.field-name-field-total-uds input.form-text', modulo).val(total_uds);
+              $('.field-name-field-total-min-prod input.form-text', modulo).val(total_min_prod);
+
+              // --------------------- Calculando el total de Horas reales
+
+              var total_real_horas = 0;
+              $('.field-name-field-operadores  > div > div > table.field-multiple-table > tbody > tr', modulo).each(function (ind, elem) {
+                var operador = this;
+
+                var real_horas = parseInt($('.field-name-field-real-horas input.form-text', operador).val());
+
+                total_real_horas += real_horas;
+
+                if ((ind + 1) === limite_operadores) {
+                  // Guarda el total de horas reales
+                  $('.field-name-field-total-real-horas input.form-text', modulo).val(total_real_horas);
+
+                  // Calcula la EFICIENCIA, previo a calcular el valor a pagar
+                  var eficiencia = parseFloat(total_min_prod / parseFloat(total_real_horas * 60));
+                  $('.field-name-field-eficiencia input.form-text', modulo).val(eficiencia);
+
+
+                  // --------------------- Calculando el Valor a Pagar
+                  var total_us;
+                  $('.field-name-field-operadores  > div > div > table.field-multiple-table > tbody > tr', modulo).each(function (ind_, elem_) {
+                    var operador_ = this;
+
+                    var real_horas_ = parseInt($('.field-name-field-real-horas input.form-text', operador_).val());
+
+                    var us = parseFloat(eficiencia * valor_minuto * real_horas_ * 60);
+
+                    $('.field-name-field-us input.form-text', operador_).val(us);
+                    total_us += parseFloat(us);
+
+                    if ((ind_ + 1) === limite_operadores) {
+                      $('.field-name-field-total-us input.form-text', modulo).val(total_us);
+                    }
+                  });
+                }
+              });
+            }
+          });
+        }
+
+        function calcularPanel(panel, valor_minuto) {
+          $('> .field-type-field-collection > div > div > table.field-multiple-table > tbody > tr', panel).each(function () {
+            var modulo = this;
+            calcularModulo(modulo, panel, valor_minuto);
+          });
+        }
+
+        function getValorMinuto () {
+
+        }
+
+        function getGrupo(panel_parent) {
+          if ($(panel_parent).hasClass('group_corte')) {
+            return 'corte';
+          }
+          if ($(panel_parent).hasClass('group_confeccion')) {
+            return 'confeccion';
+          }
+          if ($(panel_parent).hasClass('group_preparacion')) {
+            return 'preparacion';
+          }
+          if ($(panel_parent).hasClass('group_terminado')) {
+            return 'terminado';
+          }
+          if ($(panel_parent).hasClass('group_control-de-calidad')) {
+            return 'control-de-calidad';
+          }
+          if ($(panel_parent).hasClass('group_empaque')) {
+            return 'empaque';
+          }
+        }
+
+        function setAsDecimal(element) {
+          $(element).keydown(function (el)
+          {
+            var key = el.charCode || el.keyCode || 0;
+            // allow backspace, tab, delete, enter, arrows, numbers and keypad numbers ONLY
+            // home, end, period, and numpad decimal
+            return (
+            key == 8 ||
+            key == 9 ||
+            key == 13 ||
+            key == 46 ||
+            key == 110 ||
+            key == 188 ||
+            key == 190 ||
+            (key >= 35 && key <= 40) ||
+            (key >= 48 && key <= 57) ||
+            (key >= 96 && key <= 105));
+          });
+        };
+
         //Actualiza textos de los botones
         if (!$('body').hasClass('page-inicio')) {
-          var cat = ['crt', 'cnf', 'prp', 'trm', 'cnt', 'mpq'];
+
           setInterval(function(){
             for (var i = 0; i < cat.length; i++) {
               $('.field-type-field-collection.field-name-field-' + cat[i] + '-modulos > div > div > div > button.btn-info:not(.procesado)').removeClass('btn-info').addClass('btn-success procesado').text('+ Módulo');
@@ -26,45 +237,7 @@
           year: d.getFullYear()
         });
         
-        //GUARDAR DIARIO
-        $('span.guardar-diario').click(function () {
-          var data = {info: []};
 
-          $('.views-table.table-diario').each(function (i, table) {
-            var mod_id = $('tbody tr.item-diario', table).attr('data-mod-id');
-
-            data.info[i] = {mod_id: mod_id, empleados: [], tarjetas: '', vxm: ''};
-
-            $('tbody tr.item-diario', table).each(function (ind, tr) {
-              nid = $('td.views-field-nid', tr).text().trim();
-              vxm = $('input#vxm_' + nid, tr).val();
-              ctp = $('input#ctp_' + nid, tr).val();
-              rh = $('input#rh_' + nid, tr).val();
-              uds = $('input#uds_' + nid, tr).val();
-
-              data.info[i].tarjetas = ctp;
-              data.info[i].vxm = vxm;
-              data.info[i].uds = uds;
-
-              data.info[i].empleados[ind] = {
-                emp_id: nid,
-                rh: rh
-              };
-            });
-
-            if ((i + 1) === $('.views-table.table-diario').length) {
-              $.ajax({
-                type: "POST",
-                url: 'guardar-diario',
-                data: data,
-                success: function (resp) {
-                  console.log(resp);
-                },
-              });
-            }
-
-          });
-        });
 
 
         //Deshabilita campos del diario q tienen valores repetidos
